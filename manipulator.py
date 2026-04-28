e329336384a52d4525bbcd82db96238ca425ac50
Merizio <meriziofrancisco@gmail.com>
2026-04-27 21:32:42 -0300

    criação do simulador de partida da NBA

--- manipulator.py
+++ manipulator.py
@@ -1,27 +1,39 @@
 import pandas as pd
+import random
 
 class Jogador:
-    def __init__(self, Player, Age, Team, Pos, Rank):
+    def __init__(self, Player, Age, Team, Pos, Rank, FG, P3):
         self.Player = Player
         self.Age = Age
         self.Team = Team
         self.Pos = Pos
+        self.FG = FG
+        self.P3 = P3
+        self.pts = 0
         self.Rank = Rank
 
     def exibir_jogador(self):
-        print(f"{self.Player:>25} | {self.Pos}")
+        print(f"{self.Player:>25} | {self.Pos:<2} | {self.pts}")
         
 
 class Time:
     def __init__(self, nome, elenco):
         self.nome = nome
-        self.elenco = []
+        self.titulares = []
+        self.reservas = []
+        self.pontos = 0
 
-        self.elenco = [Jogador(r.Player, r.Age, r.Team, r.Pos, r.Rank) for r in elenco.itertuples()]
+        todos = [Jogador(r.Player, r.Age, r.Team, r.Pos, r.Rank, r.FG, r.P3) for r in elenco.itertuples()]
+        self.titulares = todos[:5]
+        self.reservas = todos[5:]
+
 
     def exibir_time(self):
-        print(f"\n{self.nome}")
-        for n in self.elenco:
+        print(f"\n{self.nome}\nTitulares:")
+        for n in self.titulares:
+            n.exibir_jogador()
+        print("Reservas:")
+        for n in self.reservas:
             n.exibir_jogador()
 
 
@@ -38,6 +50,21 @@ def escolher_times(nba):
     return t1, t2
 
 def exibir_partida(time1, time2):
-    print(f"PARTIDA DE HOJE:{' '*2}{time1.nome} X {time2.nome}")
+    print(f"PARTIDA DE HOJE:{' '*2}{time1.nome} {time1.pontos} X {time2.pontos} {time2.nome}")
     time1.exibir_time()
     time2.exibir_time()
+
+def escolher_jogador(elenco, modo):
+    
+    if(modo == "equal"):
+        return random.choice(elenco)
+    
+    pesos = []
+    for n in elenco:
+        if(modo == "min"):
+            pesos.append(11-n.Rank)
+        elif(modo == "max"):
+            pesos.append(n.Rank)
+        
+    jogador = random.choices(elenco, weights=pesos, k=1)[0]
+    return jogador
--- simulator.py
+++ simulator.py
@@ -1,5 +1,7 @@
 import manipulator
 import pandas as pd
+import random
+import time as tempo
 
 """
 SIMULADOR DE UM JOGO DA NBA
@@ -9,9 +11,70 @@ DISCIPLINA DE SIST. ORIENTADOS A EVENTO
 """
 
 #ESCOLHA DOS TIMES PARA O SIMULADOR
-csv=pd.read_csv("./data/nbaplayersREFORMED.csv")
+csv=pd.read_csv("./data/list_nba_players.csv")
+
 csv = csv.drop(columns=csv.columns[0])
 
 time_A, time_B = manipulator.escolher_times(csv)
 
+manipulator.exibir_partida(time_A, time_B)
+
+time_ataque, time_defesa = time_A, time_B
+#COMEÇAR O JOGO
+print("\nComeça a Partida")
+for i in range(4):
+    print(f"\n\nInício do {i+1}o quarto!  {time_A.nome} {time_A.pontos} X {time_B.pontos} {time_B.nome}")
+    posses = random.randint(60, 80)
+
+    for n in range(posses):
+        #ALEATORIZACAO DA JOGADA
+        jogada = random.random()
+
+        #SUBTITUICAO
+        if(0.236<jogada<0.296):
+            jogador1 = manipulator.escolher_jogador(time_ataque.titulares, 'min')
+            jogador2 = manipulator.escolher_jogador(time_ataque.reservas, 'max')
+            print(f"Mudança do {time_ataque.nome}: Sai {jogador1.Player} e entra {jogador2.Player}!")
+            
+            time_ataque.titulares.remove(jogador1)
+            time_ataque.reservas.remove(jogador2)
+            time_ataque.titulares.append(jogador2)
+            time_ataque.reservas.append(jogador1)
+
+        #TURNOVER
+        if(jogada<0.136):
+            jogador = manipulator.escolher_jogador(time_ataque.titulares, 'max')
+            print(f"O jogador {jogador.Player} do {time_ataque.nome} acaba de cometer um Turnover!")
+        
+        #FALTA
+        elif(0.136<jogada<0.236):
+            jogador1 = manipulator.escolher_jogador(time_ataque.titulares, 'equal')
+            jogador2 = manipulator.escolher_jogador(time_defesa.titulares, 'equal')
+            print(f"Falta cometida pelo {jogador1.Player} no {jogador2.Player}! Bola pro {time_defesa.nome}!")
+
+        #TENTATIVA DE ARREMESSO
+        else:
+            jogador = manipulator.escolher_jogador(time_ataque.titulares, 'max')
+            chance = random.random()
+            if(chance<0.585):
+                #2 PONTOS
+                chance = random.random()
+                if(chance <= jogador.FG):
+                    print(f"Jogador {jogador.Player} do {time_ataque.nome} fez uma cesta de 2 Pontos!")
+                    time_ataque.pontos+=2
+                    jogador.pts+=2
+            else:
+                #3 PONTOS
+                chance = random.random()
+                if(chance <= jogador.P3):
+                    print(f"Jogador {jogador.Player} do {time_ataque.nome} fez uma cesta de 3 Pontos!")
+                    time_ataque.pontos+=3
+                    jogador.pts+=3
+
+        #INVERSAO DA POSSE
+        time_ataque, time_defesa = time_defesa, time_ataque
+        #tempo.sleep(0.1)
+
+#STATUS FINAL DA PARTIDA
+print("\nFinal de Partida!\n")
 manipulator.exibir_partida(time_A, time_B)
